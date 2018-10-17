93742fe2c793e6b3cafcbb7f5cd51d6bba71c7de
Bzl <362220796@qq.com>
2018-10-17 13:52:05 +0800

    添加XLua脚本目录

---
+++ Assets/Game/Script/StrayFogXLuaManager.cs
@@ -0,0 +1,20 @@
+﻿using XLua;
+public class StrayFogXLuaManager : AbsSingleMonoBehaviour
+{
+    /// <summary>
+    /// lua引擎
+    /// </summary>
+    public LuaEnv luaEnv  { get; private set; }
+
+    #region OnAfterConstructor
+    /// <summary>
+    /// OnAfterConstructor
+    /// </summary>
+    protected override void OnAfterConstructor()
+    {
+        luaEnv = new LuaEnv();
+        base.OnAfterConstructor();
+    }
+    #endregion
+
+}
