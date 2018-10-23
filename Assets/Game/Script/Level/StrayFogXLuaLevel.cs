b47288a29ee3235c1c14f2468dafb596bf3ca0c7
Bzl <362220796@qq.com>
2018-10-23 14:33:56 +0800

    关键性配置XLuaGenConfig.cs

--- Assets/Game/Script/Level/StrayFogXLuaLevel.cs
+++ Assets/Game/Script/Level/StrayFogXLuaLevel.cs
@@ -1,14 +1,95 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using XLua;
+
+[System.Serializable]
+public class Injection
+{
+    public string name;
+    public GameObject value;
+}
+
 [AddComponentMenu("Game/StrayFogXLuaLevel")]
+[LuaCallCSharp]
 public class StrayFogXLuaLevel : AbsLevel
 {
+    public TextAsset luaScript;
+    public Injection[] injections;
+    internal static LuaEnv luaEnv = new LuaEnv(); //all lua behaviour shared one luaenv only!
+    internal static float lastGCTime = 0;
+    internal const float GCInterval = 1;//1 second 
+
+    private Action luaStart;
+    private Action luaUpdate;
+    private Action luaOnDestroy;
+
+    private LuaTable scriptEnv;
     /// <summary>
     /// Awake
     /// </summary>
     void Awake()
     {
-        StrayFogGamePools.xLuaManager.luaEnv.DoString("CS.UnityEngine.Debug.Log('hello world')");
-        StrayFogGamePools.xLuaManager.luaEnv.DoString("CS.UnityEngine.Debug.Log(CS.StrayFogGamePools.setting.assetBundleRoot)");
-        StrayFogGamePools.xLuaManager.luaEnv.Dispose();
+        scriptEnv = luaEnv.NewTable();
+
+        // 为每个脚本设置一个独立的环境，可一定程度上防止脚本间全局变量、函数冲突
+        LuaTable meta = luaEnv.NewTable();
+        meta.Set("__index", luaEnv.Global);
+        scriptEnv.SetMetaTable(meta);
+        meta.Dispose();
+
+        scriptEnv.Set("self", this);
+        foreach (var injection in injections)
+        {
+            scriptEnv.Set(injection.name, injection.value);
+        }
+
+        luaEnv.DoString(luaScript.text, "StrayFogXLuaLevel", scriptEnv);
+
+        Action luaAwake = scriptEnv.Get<Action>("awake");
+        scriptEnv.Get("start", out luaStart);
+        scriptEnv.Get("update", out luaUpdate);
+        scriptEnv.Get("ondestroy", out luaOnDestroy);
+
+        if (luaAwake != null)
+        {
+            luaAwake();
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        if (luaStart != null)
+        {
+            luaStart();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (luaUpdate != null)
+        {
+            luaUpdate();
+        }
+        if (Time.time - StrayFogXLuaLevel.lastGCTime > GCInterval)
+        {
+            luaEnv.Tick();
+            StrayFogXLuaLevel.lastGCTime = Time.time;
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (luaOnDestroy != null)
+        {
+            luaOnDestroy();
+        }
+        luaOnDestroy = null;
+        luaUpdate = null;
+        luaStart = null;
+        scriptEnv.Dispose();
+        injections = null;
     }
 }
+//ArgumentException: method arguments are incompatible
