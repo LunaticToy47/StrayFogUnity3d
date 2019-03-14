257109702d4cbef56a0065cc01c7fd482eab89cd
Bzl <362220796@qq.com>
2019-03-14 15:44:18 +0800

    【修改】添加窗口设置扩展类。

---
+++ Assets/Game/Script/SQLite/ExternalClass/XLS_Config_Table_UIWindowSetting.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+/// <summary>
+/// UIWindowSetting实体
+/// </summary>
+public partial class XLS_Config_Table_UIWindowSetting : AbsStrayFogSQLiteEntity
+{
+    /// <summary>
+    /// 窗口绘制模式
+    /// </summary>
+    public RenderMode winRenderMode { get; private set; }
+    /// <summary>
+    /// 窗口打开模式
+    /// </summary>
+    public enUIWindowOpenMode winOpenMode { get; private set; }
+    /// <summary>
+    /// 窗口关闭模式
+    /// </summary>
+    public enUIWindowCloseMode winCloseMode { get; private set; }
+    /// <summary>
+    /// OnResolve
+    /// </summary>
+    protected override void OnResolve()
+    {
+        winRenderMode = (RenderMode)renderMode;
+        winOpenMode = (enUIWindowOpenMode)openMode;
+        winCloseMode = (enUIWindowCloseMode)closeMode;
+    }
+}
