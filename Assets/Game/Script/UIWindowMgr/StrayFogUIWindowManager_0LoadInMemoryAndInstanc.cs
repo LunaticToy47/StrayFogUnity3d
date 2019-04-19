d10a595d08a0c344de89406eac22812b7e22b307
Bzl <362220796@qq.com>
2019-04-19 10:59:01 +0800

    【修改】测试代码。

--- Assets/Game/Script/UIWindowMgr/StrayFogUIWindowManager_0LoadInMemoryAndInstanc.cs
+++ Assets/Game/Script/UIWindowMgr/StrayFogUIWindowManager_0LoadInMemoryAndInstanc.cs
@@ -277,6 +277,7 @@ public partial class StrayFogUIWindowManager
     void OnCloseWindow<W>(XLS_Config_Table_UIWindowSetting[] _winCfgs, UIWindowEntityEventHandler<W> _callback, params object[] _parameters)
         where W : AbsUIWindowView
     {
+
         List<int> closeWinIds = new List<int>();
         Dictionary<int, int> sameLayerLessThenSiblingIndex = new Dictionary<int, int>();
         List<int> lessThenSiblingIndex = new List<int>();
