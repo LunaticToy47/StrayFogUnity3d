a07aa10deb12d255575e2934f680efb8b5f72734
baozhenliang <362220796@qq.com>
2019-07-19 14:42:52 +0800

    【添加】 MenuCommand菜单。

---
+++ Assets/Editor/StrayFog/EditorMenuCommand/EditorMenuCommand.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEditor;
+/// <summary>
+/// EditorMenuCommand
+/// </summary>
+public class EditorMenuCommand
+{
+    [MenuItem("CONTEXT/MonoBehaviour/Auto GUID")]
+    static void AutoGUID(MenuCommand menuCommand)
+    {
+        EditorStrayFogApplication.CopyToClipboard(Guid.NewGuid().ToString().UniqueHashCode().ToString());
+        EditorUtility.DisplayDialog("Auto GUID", "New GUID CopyToClipboard.", "OK");
+    }
+}
