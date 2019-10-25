3765be0421e0f7b9fbfae2e13010ee9e0e36e671
baozhenliang <362220796@qq.com>
2019-10-25 17:29:13 +0800

    【修改】BUG修改。

--- StrayFogUnity/Assets/Game/StrayFog/Guide/StrayFogGuideManager_Trigger.cs
+++ StrayFogUnity/Assets/Game/StrayFog/Guide/StrayFogGuideManager_Trigger.cs
@@ -12,7 +12,7 @@ public partial class StrayFogGuideManager
     /// <summary>
     /// 打开窗口检测
     /// </summary>
-    /// <param name="_windows">窗口组</param>
+    /// <param name="_windows">窗口</param>
     /// <param name="_parameters">参数组</param>
     public void OpenWindowCheck(AbsUIWindowView[] _windows, params object[] _parameters)
     {
